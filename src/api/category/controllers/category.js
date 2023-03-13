30a38dc9ae1359edda5e95c83d89fd2b73dc88ee
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2023-03-13 12:10:24 +0200

    optimization test

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -6,4 +6,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::category.category');
+// module.exports = createCoreController('api::category.category');
+module.exports = createCoreController('api::category.category', ({ strapi }) => ({
+    async findOne(ctx) {
+        const qp = await this.sanitizeParams(ctx);
+        const { results, pagination } = await strapi.service('api::category.category').find(qp, []);
+        const sanitizedResults = await this.sanitizeOutput(results, ctx, []);
+
+        return this.transformResponse(sanitizedResults, { pagination });
+    }
+}));
