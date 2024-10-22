ed9fb389061e8eccce8b196e8b1470fe3dfe728f
Danielius Zajenckauskas <dan@Danieliuss-MacBook-Pro.local>
2024-10-22 13:56:16 +0300

    send contact-form as mail

--- src/api/contact-form/controllers/contact-form.js
+++ src/api/contact-form/controllers/contact-form.js
@@ -1,9 +1,46 @@
 'use strict';
 
+
 /**
  * contact-form controller
- */
+*/
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true,
+    auth: {
+        user: process.env.SMTP_USERNAME,
+        pass: process.env.SMTP_PASSWORD,
+    },
+});
+
 
 const { createCoreController } = require('@strapi/strapi').factories;
+module.exports = createCoreController('api::contact-form.contact-form', ({ strapi }) => ({
+    async create(ctx) {
+        const { data } = await super.create(ctx);
+        const emailContent = `
+        <h1>New Contact Form Submission</h1>
+        <p><strong>Date:</strong> ${data.attributes.createdAt}</p>
+        <p><strong>Name:</strong> ${data.attributes.name}</p>
+        <p><strong>Email:</strong> ${data.attributes.email}</p>
+        <p><strong>Message:</strong> ${data.attributes.message}</p>
+    `;
+        async function main() {
+            const info = await transporter.sendMail({
+                to: ['info@opklinika.lt', '1000kaktusu@gmail.com'],
+                from: 'info@opklinika.lt',
+                subject: `NAUJA užklausos forma užpildyta per OPKLINIKA.LT: ${data.attributes.name}`,
+                html: emailContent
+            });
+            console.log("Message sent: %s", info.messageId);
+        }
 
-module.exports = createCoreController('api::contact-form.contact-form');
+        main().catch(console.error);
+
+
+        return { data };
+    }
+}));
